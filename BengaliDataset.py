ea34670110f529f8967334c32346a03545367dfc
soosiey <yerapathisuhaas@gmail.com>
2020-04-15 20:20:42 -0500

    working dataset

--- BengaliDataset.py
+++ BengaliDataset.py
@@ -1,9 +1,52 @@
 import numpy as np
-import torch.utils.data.Dataset
+import torch
+from  torch.utils.data import Dataset
 import pyarrow.parquet as pq
-import pandas
-
+import pandas as pd
+#import torchvision
 class BengaliDataset(Dataset):
 
-    def __init__(self, csvfile, parquetDir, train=False):
-        pass
+    def __init__(self, csvfile, parquetDir, train=True, transform=None):
+        if(train):
+            print("Initializing training dataset...")
+        else:
+            print("Initializing testing dataset...")
+
+        self.labels = pd.read_csv(parquetDir + csvfile)
+        self.root_dir = parquetDir
+        self.transform = transform
+        base_str = 'train_image_data_'
+        filetype = '.parquet'
+
+        startpoint = 0 if train else 3
+
+        self.data = pq.read_pandas(self.root_dir  + base_str + str(startpoint) + filetype).to_pandas()
+        if(train):
+            print("1 out of 3 frames finished")
+        else:
+            print("Done initializing")
+        if(train):
+            d1 = pq.read_pandas(self.root_dir  + base_str + str(startpoint + 1) + filetype).to_pandas()
+            print("2 out of 3 frames finished")
+            d2 = pq.read_pandas(self.root_dir  + base_str + str(startpoint + 2) + filetype).to_pandas()
+            self.data = pd.concat([self.data, d1,d2], ignore_index=True)
+            print("Done initializing")
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self,idx):
+        if(torch.is_tensor(idx)):
+            idx = idx.tolist()
+
+
+        a = self.data.iloc[idx,1:]
+        x =a.to_numpy()
+        x = x.astype('float').reshape(-1,137,236)
+        y = self.labels.iloc[idx,1:4]
+        y = y.to_numpy()
+        y = y.reshape(-1,3)
+
+        if(self.transform):
+            x = self.transform(x)
+        return {'data':x, 'labels':y}
+
--- image_test.py
+++ image_test.py
@@ -1,14 +1,24 @@
 import pyarrow.parquet as pq
 import numpy as np
 import matplotlib.pyplot as plt
+from BengaliDataset import BengaliDataset
 
-imgs = pq.read_pandas('./dataset/train_image_data_1.parquet').to_pandas()
+#imgs = pq.read_pandas('./dataset/train_image_data_1.parquet').to_pandas()
+#
+#test = imgs.iloc[0]
+#test = np.array(test)
+#test = test[1:]
+#test = test.reshape((137,236)).astype(int)
+#
+#plt.figure()
+#plt.imshow(test,cmap='gray', vmin=0, vmax=255)
+#plt.show()
 
-test = imgs.iloc[0]
-test = np.array(test)
-test = test[1:]
-test = test.reshape((137,236)).astype(int)
-
+d = BengaliDataset('train.csv', './dataset/')
+print(d.data.shape)
+print(d[[0,1]]['data'].shape)
+print(d[[0,1]]['labels'])
+sample = d[5]['data'][0]
 plt.figure()
-plt.imshow(test,cmap='gray', vmin=0, vmax=255)
+plt.imshow(sample.astype(int), cmap='gray', vmin=0, vmax=255)
 plt.show()
