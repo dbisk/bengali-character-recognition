eb3d231b9558962068f807b00de5cf26df21ad52
dbisk <16860792+dbisk@users.noreply.github.com>
2020-05-05 01:52:12 -0700

    Added some files/structure

---
+++ train.py
@@ -0,0 +1,11 @@
+
+from tqdm import tqdm
+import torch
+
+# import model
+# from models import resnet
+
+def train(model, train_dataloader, test_dataloader):
+  for i, batch in tqdm(enumerate(train_dataloader)):
+    pass
+  return
