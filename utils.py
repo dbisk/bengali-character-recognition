4f00dab988bafcbcbaa3c3edb4038af290db1f0b
dbisk <16860792+dbisk@users.noreply.github.com>
2020-05-05 01:55:08 -0700

    Moved to 1-time parquet read only, future runs use .pkl instead. Shuffling works, but still spikes at momentary high memory."

--- utils.py
+++ utils.py
@@ -1,34 +1,62 @@
-import pyarrow.parquet as pq
+# import built-in modules
+import time
+from multiprocessing import Pool
+
+# import 3rd-party modules
 import pandas as pd
-from BengaliDataset import BengaliDataset as bd
+
+# import custom modules
+from BengaliDataset import BengaliDataset
 import train
-import numpy as np
 
+# def test_fn(filepath):
+#     return pd.read_parquet(filepath, engine='pyarrow')
 
-def getData(root_dir, csvfile, train_transform=None, test_transform=None):
-
+def dsetToPickle(root_dir, csvfile):
+    """Converts the parquet files to pickle format for faster io"""
     base_str = 'train_image_data_'
-    filetype = '.parquet'
-    labels = pd.read_csv(root_dir + csvfile)
-    data1 = pq.read_pandas(root_dir + base_str + '0' + filetype).to_pandas()
-    print('First parquet read')
-    data2 = pq.read_pandas(root_dir + base_str + '1' + filetype).to_pandas()
-    print('Second parquet read')
-    data3 = pq.read_pandas(root_dir + base_str + '2' + filetype).to_pandas()
-    print('Third parquet read')
-    data4 = pq.read_pandas(root_dir + base_str + '3' + filetype).to_pandas()
-    print('Fourth parquet read')
-    data = pd.concat([data1, data2, data3, data4])
-    print('All data loaded')
+    labels = pd.read_csv(root_dir + csvfile).set_index('image_id', drop=True)
+    filepaths = []
+    for i in range(4):
+        filepaths.append(root_dir + base_str + str(i) + '.parquet')
+    start_time = time.time()
+    print("Reading parquet files...")
+    # with Pool(processes=4) as pool:
+    #     data = pool.map(test_fn, filepaths)
+    # print(data)
+    data = []
+    for i in range(4):
+        data.append(pd.read_parquet(filepaths[i], engine='pyarrow').set_index('image_id', drop=True))
+        print("Loaded parquet file " + str(i) + "...")
+    data = pd.concat(data, copy=False)
+    print("Parquet loading completed. Elapsed: %d seconds" % (time.time() - start_time))
+
+    # insert labels to the front of the dataframe
+    labels = labels.iloc[:len(data)]
     data.insert(0, 'grapheme_root', labels['grapheme_root'])
     data.insert(1, 'vowel_diacritic', labels['vowel_diacritic'])
     data.insert(2, 'consonant_diacritic', labels['consonant_diacritic'])
+    del(labels) # probably not required, but clears up some memory
 
-    del(labels)
-    del(data1, data2, data3, data4)
-    data.astype(np.int8, errors='ignore', copy=False)
-    train = bd(data.iloc[:160672], transform=train_transform)
-    test = bd(data.iloc[160672:], transform=test_transform)
+    # shuffle
+    data = data.sample(frac=1)
+
+    # save to pickle file
+    pkl_path = root_dir + "full_data.pkl"
+    data.to_pickle(pkl_path)
+    del(data)
+    return pkl_path
+
+def getData(pickle_path, split=0.8, train_transform=None, test_transform=None):
+    start_time = time.time()
+    print("Reading pickle from " + pickle_path + "...")
+    data = pd.read_pickle(pickle_path)
+    print("Pickle loading completed. Elapsed: %d seconds" % (time.time() - start_time))
+    split_index = int(split * len(data))
+
+    # create the BengaliDataset objects
+    train = BengaliDataset(data.iloc[:split_index], transform=train_transform)
+    test = BengaliDataset(data.iloc[split_index:], transform=test_transform)
     return train, test
 
 
