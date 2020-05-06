7ce2c235f6fa00ce41f62804c58c5e87e11339b6
Suhaas Yerapathi <yerapathisuhaas@gmail.com>
2020-05-06 19:56:21 -0400

    Revert "Moved to 1-time parquet read only, future runs use .pkl instead."

--- testing.py
+++ testing.py
@@ -1,39 +1,32 @@
 import matplotlib.pyplot as plt
-from utils import getData, trainModel, dsetToPickle
+from utils import getData, trainModel
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
-def testDataset(need_pickle=False):
-    if (need_pickle):
-        pkl_path = dsetToPickle('./dataset/', 'train.csv')
-    else:
-        pkl_path = './dataset/full_data.pkl'
-    train, test = getData(pkl_path)
+def testDataset():
+    train, test = getData('./dataset/', 'train.csv')
     print('Testing...')
-    print("train.data:\n", train.data)
-    print("train[0]:\n", train[0])
+    print(train.data.iloc[0])
+    print(train[0])
+    print(train[[0,1]]['data'].shape)
+    print(train[[0,1]]['labels'])
+    print(train[[0,1]]['identifier'])
     for i in range(5):
         plt.subplot(150 + i + 1)
-        sample = train[i]['data'][0]
-        plt.imshow(sample.astype(int), cmap='gray', vmin=0, vmax=255)
+        sample = train[i]['data'][0][0]
+        plt.imshow(sample.astype(int),cmap='gray', vmin=0, vmax=255)
     plt.show()
 
 
 
-def testTrainingLoop(need_pickle=False):
-    if (need_pickle):
-        pkl_path = dsetToPickle('./dataset/', 'train.csv')
-    else:
-        pkl_path = './dataset/full_data.pkl'
+def testTrainingLoop():
 
     main_transform = transforms.Compose([transforms.ToTensor()])
-    trainset, testset = getData(pkl_path, train_transform = main_transform, test_transform = main_transform)
+    trainset, testset = getData('./dataset/', 'train.csv', train_transform = main_transform, test_transform = main_transform)
     train_dataloader = DataLoader(trainset, batch_size = 64, shuffle = True, num_workers = 8)
     test_dataloader = DataLoader(testset, batch_size = 64, shuffle = True, num_workers = 8)
     trainModel(None, train_dataloader, test_dataloader)
 
 
-if __name__ == '__main__':
-    # set this to true if you don't already have a pickle file
-    testDataset(need_pickle=False)
-    # testTrainingLoop(need_pickle=False)
+#testDataset()
+testTrainingLoop()
