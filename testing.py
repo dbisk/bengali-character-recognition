a061b477bd33ae19f6b9597e585982f709e90352
soosiey <yerapathisuhaas@gmail.com>
2020-05-10 20:29:42 -0500

    added vis function testing

--- testing.py
+++ testing.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from utils import getData, trainModel, dsetToPickle
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from models import customModel1 as custom
+import torch
 
 def testDataset(need_pickle=False):
     if (need_pickle):
@@ -32,8 +34,28 @@ def testTrainingLoop(need_pickle=False):
     test_dataloader = DataLoader(testset, batch_size = 64, shuffle = True)
     trainModel(None, train_dataloader, test_dataloader)
 
+def visualize():
+
+    pkl_path = './dataset/full_data.pkl'
+    main_transform = transforms.Compose([transforms.ToTensor()])
+    trainset, testset = getData(pkl_path, train_transform = main_transform, test_transform = main_transform)
+    test_dataloader = DataLoader(testset, batch_size = 1, shuffle = True)
+    model = custom.CustomNet()
+    plt.figure(dpi=300)
+    for i,batch in enumerate(test_dataloader):
+        if(i <5):
+            plt.subplot(150 + i + 1)
+            data = test_dataloader[i]
+            img = data['data']
+            labels = data['labels']
+            outs = model(img)
+            _,preds = torch.max(outs,1)
+            print(labels, preds)
+        else:
+            break
 
 if __name__ == '__main__':
     # set this to true if you don't already have a pickle file
     #testDataset(need_pickle=False)
-     testTrainingLoop(need_pickle=False)
+    #testTrainingLoop(need_pickle=False)
+    visualize()
